c262bd3ca9ede261de02de343db61742d74ce602
Nishant Tilve <nishanttilve@gmail.com>
2022-03-06 16:23:23 +0530

    feat: set up prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const helmet = require("helmet");
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
 
-const { PORT } = require("./config");
+const { PORT } = require('./config')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(helmet());
-app.use(morgan('tiny'));
-app.use(express.json());
+app.use(cors())
+app.use(helmet())
+app.use(morgan('tiny'))
+app.use(express.json())
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+	console.log(`Listening on port: ${PORT}`)
 })
