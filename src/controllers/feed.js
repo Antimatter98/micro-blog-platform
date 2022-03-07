faad46ea36d88a599812b074d64db133b7c4f941
Nishant Tilve <nishanttilve@gmail.com>
2022-03-07 13:29:51 +0530

    feat: add pagination and data limitation to the feed api

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -3,11 +3,17 @@ const User = require('../models/user')
 
 const feedRouter = require('express').Router()
 
-feedRouter.get('/', async (req, res) => {
+feedRouter.get('/:page?', async (req, res) => {
 	const id = req.user?.id
+	const page = req.params?.page || 1
+	const limit = req.query?.limit || 10
 
 	const user = await User.findById(id).exec()
-	const posts = await Post.find({}, '-_id').populate('creator', 'name email').exec()
+	const posts = await Post.find({}, '-_id')
+		.skip((page - 1) * limit)
+		.limit(limit)
+		.populate('creator', 'name email')
+		.exec()
 	// show posts from people you follow
 	const postsFromFollowing = posts.filter(post => user.following?.includes(post.creator?._id))
 
